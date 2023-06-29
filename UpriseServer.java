2c7b0104ca668bd485334f1bacb581bcad013ab4
conrad-coder <cddavid2001@gmail.com>
2023-06-30 00:09:22 +0300

    updated code with the deposit function fully  working

--- UpriseServer.java
+++ UpriseServer.java
@@ -4,10 +4,17 @@ import java.net.Socket;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class UpriseServer {
     private static final int PORT = 12345;
     private List<Socket> clients;
+    private Connection connection;
 
     public UpriseServer() {
         clients = new ArrayList<>();
@@ -25,7 +32,7 @@ public class UpriseServer {
                 clients.add(clientSocket);
                 ClientHandler clientHandler = new ClientHandler(clientSocket);
                 new Thread(clientHandler).start();
-                serverSocket.close();
+
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -45,8 +52,24 @@ public class UpriseServer {
         }
     }
 
-    public static void main(String[] args) {
+    public void databaseConnect() throws IOException {
+        String url = "jdbc:mysql://localhost:3306/uprise sacco";
+        String username = "root";
+        String password = "";
+
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection to database successful");
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
         UpriseServer server = new UpriseServer();
+        server.databaseConnect();
         server.start();
     }
 
@@ -81,7 +104,7 @@ public class UpriseServer {
                         String username = commandArgs[1];
                         String password = commandArgs[2];
 
-                        // Simulated login verification
+                        // login verification
                         if (verifyLogin(username, password)) {
                             writer.println("Login successful.");
                             loggedIn = true;
@@ -95,21 +118,21 @@ public class UpriseServer {
                             LocalDate dateDeposited = LocalDate.parse(commandArgs[2]);
                             int receiptNumber = Integer.parseInt(commandArgs[3]);
 
-                            // Simulated deposit processing
+                            // deposit processing
                             processDeposit(amount, dateDeposited, receiptNumber);
                             writer.println("Deposit submitted successfully.");
                         } else if (command.equals("checkStatement")) {
                             LocalDate dateFrom = LocalDate.parse(commandArgs[1]);
                             LocalDate dateTo = LocalDate.parse(commandArgs[2]);
 
-                            // Simulated statement generation
+                            // statement generation
                             String statement = generateStatement(dateFrom, dateTo);
                             writer.println(statement);
                         } else if (command.equals("requestLoan")) {
                             double amount = Double.parseDouble(commandArgs[1]);
                             int paymentPeriod = Integer.parseInt(commandArgs[2]);
 
-                            // Simulated loan request processing
+                            // loan request processing
                             String loanApplicationNumber = processLoanRequest(amount, paymentPeriod);
                             writer.println("Loan application number: " + loanApplicationNumber);
                         } else if (command.equals("loanRequestStatus")) {
@@ -129,11 +152,10 @@ public class UpriseServer {
                                         String response = reader.readLine();
 
                                         if (response.equals("accept")) {
-                                            // Handle loan acceptance logic
                                             acceptLoan(loanApplicationNumber);
                                             writer.println("Loan accepted successfully.");
                                         } else if (response.equals("reject")) {
-                                            // Handle loan rejection logic
+
                                             rejectLoan(loanApplicationNumber);
                                             writer.println("Loan rejected.");
                                         }
@@ -156,48 +178,140 @@ public class UpriseServer {
         }
 
         private boolean verifyLogin(String username, String password) {
-            // Simulated login verification logic
-            // Replace with your own implementation
+            // try {
+            // String query = "SELECT * FROM users WHERE username = ? AND password = ?";
+            // PreparedStatement statement = connection.prepareStatement(query);
+            // statement.setString(1, username);
+            // statement.setString(2, password);
+            // ResultSet resultSet = statement.executeQuery();
+            // return resultSet.next();
+            // } catch (SQLException e) {
+            // e.printStackTrace();
+            // }
+            // return false;
             return true;
         }
 
         private void processDeposit(double amount, LocalDate dateDeposited, int receiptNumber) {
-            // Simulated deposit processing logic
-            // Replace with your own implementation
+            try {
+                String query = "INSERT INTO deposits (amount, date_deposited, receipt_number) VALUES (?, ?, ?)";
+                PreparedStatement statement = connection.prepareStatement(query);
+                statement.setDouble(1, amount);
+                statement.setDate(2, java.sql.Date.valueOf(dateDeposited));
+                statement.setInt(3, receiptNumber);
+                statement.executeUpdate();
+                System.out.println("Deposit processed successfully.");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
 
         private String generateStatement(LocalDate dateFrom, LocalDate dateTo) {
-            // Simulated statement generation logic
-            // Replace with your own implementation
-            return "Statement";
+            // StringBuilder statementBuilder = new StringBuilder();
+            // try {
+            // String query = "SELECT * FROM deposits WHERE date_deposited >= ? AND
+            // date_deposited <= ?";
+            // PreparedStatement preparedStatement = connection.prepareStatement(query);
+            // preparedStatement.setDate(1, java.sql.Date.valueOf(dateFrom));
+            // preparedStatement.setDate(2, java.sql.Date.valueOf(dateTo));
+            // ResultSet resultSet = preparedStatement.executeQuery();
+            // while (resultSet.next()) {
+            // double amount = resultSet.getDouble("amount");
+            // LocalDate dateDeposited = resultSet.getDate("date_deposited").toLocalDate();
+            // int receiptNumber = resultSet.getInt("receipt_number");
+            // statementBuilder.append("Amount: ").append(amount).append(", Date Deposited:
+            // ")
+            // .append(dateDeposited).append(", Receipt Number:
+            // ").append(receiptNumber).append("\n");
+            // }
+            // } catch (SQLException e) {
+            // e.printStackTrace();
+            // }
+            // return statementBuilder.toString();
+            return "statement";
         }
 
         private String processLoanRequest(double amount, int paymentPeriod) {
-            // Simulated loan request processing logic
-            // Replace with your own implementation
+            // String loanApplicationNumber = null;
+            // try {
+            // String query = "INSERT INTO loans (amount, payment_period) VALUES (?, ?)";
+            // PreparedStatement statement = connection.prepareStatement(query,
+            // PreparedStatement.RETURN_GENERATED_KEYS);
+            // statement.setDouble(1, amount);
+            // statement.setInt(2, paymentPeriod);
+            // statement.executeUpdate();
+
+            // ResultSet generatedKeys = statement.getGeneratedKeys();
+            // if (generatedKeys.next()) {
+            // loanApplicationNumber = String.valueOf(generatedKeys.getInt(1));
+            // }
+            // } catch (SQLException e) {
+            // e.printStackTrace();
+            // }
+            // return loanApplicationNumber;
             return "12345";
         }
 
         private String getLoanRequestStatus(String loanApplicationNumber) {
-            // Simulated loan request status retrieval logic
-            // Replace with your own implementation
+            // try {
+            // String query = "SELECT status FROM loans WHERE loan_application_number = ?";
+            // PreparedStatement statement = connection.prepareStatement(query);
+            // statement.setString(1, loanApplicationNumber);
+            // ResultSet resultSet = statement.executeQuery();
+            // if (resultSet.next()) {
+            // return resultSet.getString("status");
+            // }
+            // } catch (SQLException e) {
+            // e.printStackTrace();
+            // }
+            // return null;
+
             return "Granted";
         }
 
         private String getLoanDetails(String loanApplicationNumber) {
-            // Simulated loan details retrieval logic
-            // Replace with your own implementation
+            // StringBuilder loanDetails = new StringBuilder();
+            // try {
+            // String query = "SELECT * FROM loans WHERE loan_application_number = ?";
+            // PreparedStatement statement = connection.prepareStatement(query);
+            // statement.setString(1, loanApplicationNumber);
+            // ResultSet resultSet = statement.executeQuery();
+            // if (resultSet.next()) {
+            // double amount = resultSet.getDouble("amount");
+            // int paymentPeriod = resultSet.getInt("payment_period");
+            // loanDetails.append("Amount: ").append(amount).append(", Payment
+            // Period:").append(paymentPeriod);
+            // }
+            // } catch (SQLException e) {
+            // e.printStackTrace();
+            // }
+            // return loanDetails.toString();
+
             return "Loan Details";
         }
 
         private void acceptLoan(String loanApplicationNumber) {
-            // Simulated loan acceptance logic
-            // Replace with your own implementation
+            // try {
+            // String query = "UPDATE loans SET status = 'Accepted' WHERE
+            // loan_application_number = ?";
+            // PreparedStatement statement = connection.prepareStatement(query);
+            // statement.setString(1, loanApplicationNumber);
+            // statement.executeUpdate();
+            // } catch (SQLException e) {
+            // e.printStackTrace();
+            // }
         }
 
         private void rejectLoan(String loanApplicationNumber) {
-            // Simulated loan rejection logic
-            // Replace with your own implementation
+            // try {
+            // String query = "UPDATE loans SET status = 'Rejected' WHERE
+            // loan_application_number = ?";
+            // PreparedStatement statement = connection.prepareStatement(query);
+            // statement.setString(1, loanApplicationNumber);
+            // statement.executeUpdate();
+            // } catch (SQLException e) {
+            // e.printStackTrace();
+            // }
         }
     }
 }
